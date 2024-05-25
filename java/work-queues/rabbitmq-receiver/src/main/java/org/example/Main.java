10bffacb827f8de1be5ed37ac0cb64303a77c7b1
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-05-25 18:27:59 +0530

    rabit mq code added

---
+++ java/work-queues/rabbitmq-receiver/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    }
+}
+
